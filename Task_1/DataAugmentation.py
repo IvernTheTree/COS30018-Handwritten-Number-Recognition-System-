29376e23e22d192ed65f6d3a686ee28831b47076
Adonias Pedro <104463681@student.swin.edu.au>
2025-08-26 17:35:11 +1000

    Data Augmentation

--- Task_1/DataAugmentation.py
+++ Task_1/DataAugmentation.py
@@ -1,7 +1,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 """
 transform_train = transforms.Compose([
